6e039ba4c04279cda9eb23a69089cf2d3f7de0e5
aryadaroui <adaroui@uci.edu>
2023-07-28 16:26:36 -0700

    simplified create_dir and copy calls

---
+++ src-tauri/src/export.rs
@@ -0,0 +1,188 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+use trash;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PhotoFeedback {
+    name: String,
+    love: i8, // TODO: change this to sentiment
+    rating: i8,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ExportSettings {
+    favorite: bool,
+    star3: bool,
+    star2: bool,
+    star1: bool,
+    dislike: bool,
+    delete_original: bool,
+}
+
+pub type FeedbackArray = Vec<PhotoFeedback>;
+
+struct SubDirs {
+    love: &'static str,
+    star3: &'static str,
+    star2: &'static str,
+    star1: &'static str,
+    dislike: &'static str,
+}
+
+const SUB_DIRS: SubDirs = SubDirs {
+    love: "love",
+    star3: "star3",
+    star2: "star2",
+    star1: "star1",
+    dislike: "dislike",
+};
+
+fn create_dir(setting: bool, dir: String) -> Result<(), String> {
+    if setting {
+        match fs::create_dir_all(dir.clone()) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(format!("Failed to create {}: {}", dir, e)),
+        }
+    } else {
+        Ok(())
+    }
+}
+
+fn copy_photo(
+    condition: bool,
+    dir: String,
+    sub_dir: &str,
+    photo: PhotoFeedback,
+) -> Result<(), String> {
+    if condition {
+        match fs::copy(
+            dir.clone() + &photo.name,
+            dir.clone() + sub_dir + "/" + &photo.name,
+        ) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(format!(
+                "Failed to copy {} to ./{}: {}",
+                photo.name, sub_dir, e
+            )),
+        }
+    } else {
+        Ok(())
+    }
+}
+
+#[tauri::command]
+pub async fn export(
+    dir: String,
+    photos: FeedbackArray,
+    settings: ExportSettings,
+) -> Result<(), String> {
+    // create directories if they don't exist
+    create_dir(settings.favorite, dir.clone() + SUB_DIRS.love)?;
+    create_dir(settings.star3, dir.clone() + SUB_DIRS.star3)?;
+    create_dir(settings.star2, dir.clone() + SUB_DIRS.star2)?;
+    create_dir(settings.star1, dir.clone() + SUB_DIRS.star1)?;
+    create_dir(settings.dislike, dir.clone() + SUB_DIRS.dislike)?;
+
+    // copy photos to their respective directories
+    for photo in &photos {
+        copy_photo(
+            photo.love == 1 && settings.favorite,
+            dir.clone(),
+            SUB_DIRS.love,
+            photo.clone(),
+        )?;
+        copy_photo(
+            photo.rating == 3 && settings.star3,
+            dir.clone(),
+            SUB_DIRS.star3,
+            photo.clone(),
+        )?;
+        copy_photo(
+            photo.rating == 2 && settings.star2,
+            dir.clone(),
+            SUB_DIRS.star2,
+            photo.clone(),
+        )?;
+        copy_photo(
+            photo.rating == 1 && settings.star1,
+            dir.clone(),
+            SUB_DIRS.star1,
+            photo.clone(),
+        )?;
+        copy_photo(
+            photo.love == -1 && settings.dislike,
+            dir.clone(),
+            SUB_DIRS.dislike,
+            photo.clone(),
+        )?;
+
+        // if photo.love == 1 && settings.favorite {
+        //     match fs::copy(
+        //         dir.clone() + &photo.name,
+        //         dir.clone() + "love/" + &photo.name,
+        //     ) {
+        //         Ok(_) => {}
+        //         Err(e) => return Err(format!("Failed to copy {} to ./love: {}", photo.name, e)),
+        //     };
+        // }
+
+    //     if photo.rating == 3 && settings.star3 {
+    //         match fs::copy(
+    //             dir.clone() + &photo.name,
+    //             dir.clone() + "star3/" + &photo.name,
+    //         ) {
+    //             Ok(_) => {}
+    //             Err(e) => return Err(format!("Failed to copy {} to ./star3: {}", photo.name, e)),
+    //         };
+    //     }
+
+    //     if photo.rating == 2 && settings.star2 {
+    //         match fs::copy(
+    //             dir.clone() + &photo.name,
+    //             dir.clone() + "star2/" + &photo.name,
+    //         ) {
+    //             Ok(_) => {}
+    //             Err(e) => return Err(format!("Failed to copy {} to ./star2: {}", photo.name, e)),
+    //         };
+    //     }
+
+    //     if photo.rating == 1 && settings.star1 {
+    //         match fs::copy(
+    //             dir.clone() + &photo.name,
+    //             dir.clone() + "star1/" + &photo.name,
+    //         ) {
+    //             Ok(_) => {}
+    //             Err(e) => return Err(format!("Failed to copy {} to ./star1: {}", photo.name, e)),
+    //         };
+    //     }
+
+    //     if photo.love == -1 && settings.dislike {
+    //         match fs::copy(
+    //             dir.clone() + &photo.name,
+    //             dir.clone() + "hate/" + &photo.name,
+    //         ) {
+    //             Ok(_) => {}
+    //             Err(e) => return Err(format!("Failed to copy {} to ./hate: {}", photo.name, e)),
+    //         };
+    //     }
+    }
+
+    if settings.delete_original {
+        for photo in &photos {
+            let trashable = (photo.love == 1 && settings.favorite)
+                || (photo.rating == 3 && settings.star3)
+                || (photo.rating == 2 && settings.star2)
+                || (photo.rating == 1 && settings.star1)
+                || (photo.love == -1 && settings.dislike);
+
+            if trashable {
+                match trash::delete(dir.clone() + &photo.name) {
+                    Ok(_) => {}
+                    Err(e) => return Err(format!("Failed to move {} to trash: {}", photo.name, e)),
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,146 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
+mod export;
+
 use tauri::Manager;
-use trash;
 use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct PhotoFeedback {
-    name: String,
-    love: i8, // TODO: change this to sentiment
-    rating: i8,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ExportSettings {
-    favorite: bool,
-    star3: bool,
-    star2: bool,
-    star1: bool,
-    dislike: bool,
-    delete_original: bool,
-}
-
-type FeedbackArray = Vec<PhotoFeedback>;
-
-#[tauri::command]
-async fn export(
-    dir: String,
-    photos: FeedbackArray,
-    settings: ExportSettings,
-) -> Result<(), String> {
-    // create directories if they don't exist
-    if settings.favorite {
-        match fs::create_dir_all(dir.clone() + "love") {
-            Ok(_) => {}
-            Err(e) => return Err(format!("Failed to create ./love: {}", e)),
-        };
-    }
-
-    if settings.star3 {
-        match fs::create_dir_all(dir.clone() + "star3") {
-            Ok(_) => {}
-            Err(e) => return Err(format!("Failed to create ./star3: {}", e)),
-        };
-    }
-
-    if settings.star2 {
-        match fs::create_dir_all(dir.clone() + "star2") {
-            Ok(_) => {}
-            Err(e) => return Err(format!("Failed to create ./star2: {}", e)),
-        };
-    }
-
-    if settings.star1 {
-        match fs::create_dir_all(dir.clone() + "star1") {
-            Ok(_) => {}
-            Err(e) => return Err(format!("Failed to create ./star1: {}", e)),
-        };
-    }
-
-    if settings.dislike {
-        match fs::create_dir_all(dir.clone() + "hate") {
-            Ok(_) => {}
-            Err(e) => return Err(format!("Failed to create ./hate: {}", e)),
-        };
-    }
-
-    // copy photos to their respective directories
-    for photo in &photos {
-        if photo.love == 1 && settings.favorite {
-            match fs::copy(
-                dir.clone() + &photo.name,
-                dir.clone() + "love/" + &photo.name,
-            ) {
-                Ok(_) => {}
-                Err(e) => return Err(format!("Failed to copy {} to ./love: {}", photo.name, e)),
-            };
-        }
-
-        if photo.rating == 3 && settings.star3 {
-            match fs::copy(
-                dir.clone() + &photo.name,
-                dir.clone() + "star3/" + &photo.name,
-            ) {
-                Ok(_) => {}
-                Err(e) => return Err(format!("Failed to copy {} to ./star3: {}", photo.name, e)),
-            };
-        }
-
-        if photo.rating == 2 && settings.star2 {
-            match fs::copy(
-                dir.clone() + &photo.name,
-                dir.clone() + "star2/" + &photo.name,
-            ) {
-                Ok(_) => {}
-                Err(e) => return Err(format!("Failed to copy {} to ./star2: {}", photo.name, e)),
-            };
-        }
-
-        if photo.rating == 1 && settings.star1 {
-            match fs::copy(
-                dir.clone() + &photo.name,
-                dir.clone() + "star1/" + &photo.name,
-            ) {
-                Ok(_) => {}
-                Err(e) => return Err(format!("Failed to copy {} to ./star1: {}", photo.name, e)),
-            };
-        }
-
-        if photo.love == -1 && settings.dislike {
-            match fs::copy(
-                dir.clone() + &photo.name,
-                dir.clone() + "hate/" + &photo.name,
-            ) {
-                Ok(_) => {}
-                Err(e) => return Err(format!("Failed to copy {} to ./hate: {}", photo.name, e)),
-            };
-        }
-    }
-
-    if settings.delete_original {
-        for photo in &photos {
-            let trashable = (photo.love == 1 && settings.favorite)
-                || (photo.rating == 3 && settings.star3)
-                || (photo.rating == 2 && settings.star2)
-                || (photo.rating == 1 && settings.star1)
-                || (photo.love == -1 && settings.dislike);
-
-            if trashable {
-                match trash::delete(dir.clone() + &photo.name) {
-                    Ok(_) => {}
-                    Err(e) => return Err(format!("Failed to move {} to trash: {}", photo.name, e)),
-                }
-            }
-        }
-    }
-
-    Ok(())
-}
 
 fn main() {
     tauri::Builder::default()
@@ -153,7 +19,7 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![export])
+        .invoke_handler(tauri::generate_handler![export::export])
         .run(tauri::generate_context!())
         .expect("Fatal error while running Favy.");
 }

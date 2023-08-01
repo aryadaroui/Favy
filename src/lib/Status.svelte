f4e894754992bcd0dcd61aa89d642576efddf84e
aryadaroui <adaroui@uci.edu>
2023-08-01 10:33:45 -0700

    useless changes

--- src/lib/Status.svelte
+++ src/lib/Status.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 import {status} from '$lib/stores'
 
-
 let progress_shown = false
 
 
+$: $status.text, status_update()
 
-
-
+function status_update() {
+	progress_shown = false
+	console.log('status_update', $status.text)
+}
 
 </script>
 
